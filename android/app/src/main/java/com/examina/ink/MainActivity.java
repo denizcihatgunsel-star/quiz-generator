3d3fd194392fe8fc4202ca33ea9c32d6145ebd42
denizcihatgunsel-star <denizcihatgunsel@gmail.com>
2026-08-09 22:33:01 +0300

    chore: pin mobile user agent in app webview

--- android/app/src/main/java/com/examina/ink/MainActivity.java
+++ android/app/src/main/java/com/examina/ink/MainActivity.java
@@ -1,5 +1,19 @@
 package com.examina.ink;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Pin a mobile user agent so the site always serves the mobile version.
+        WebView webView = getBridge().getWebView();
+        String mobileUa = WebSettings.getDefaultUserAgent(this)
+                + " Mobile ExaminaApp/1.0";
+        webView.getSettings().setUserAgentString(mobileUa);
+    }
+}
